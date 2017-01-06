99623aa461c8268af1b8905c838d2270c14628ba
Fernando Nakagawa <fernandonkgw@gmail.com>
2017-01-06 16:55:42 -0200

    organizando em subrotas

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -2,6 +2,7 @@ package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -19,6 +20,13 @@ public class RotaPedidos {
 				String diretorio = "/home/developer/workspace/camel-alura/pedidos"; // poderia ser apenas "pedidos"
 				
 				from("file:" + diretorio + "?delay=5s&noop=true")
+					.routeId("rota-pedidos")
+					.multicast()
+						.to("direct:soap")
+						.to("direct:http");
+				
+				from("direct:http")
+					.routeId("rota-http")
 					.setProperty("pedidoId", xpath("/pedido/id/text()"))
 					.setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()"))
 					.split()
@@ -33,10 +41,19 @@ public class RotaPedidos {
 					.setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET))
 					.setHeader(Exchange.HTTP_QUERY, simple("clienteId=${exchangeProperty.clienteId}&pedidoId=${exchangeProperty.pedidoId}&ebookId=${exchangeProperty.ebookId}"))
 				.to("http4://localhost:8080/webservices/ebook/item");
+				
+				from("direct:soap")
+				.routeId("rota-soap")
+				.log("chamando serviço soap ${body}")
+				.to("mock:soap");
 			}
 		});
 		
 		context.start(); // aqui o camel inicia o trabalho
+		
+		ProducerTemplate producer = context.createProducerTemplate();
+	    producer.sendBody("direct:soap", "<pedido> ... </pedido>");
+		
 		Thread.sleep(3000); // espera um pouco para terminar a rota
 		context.stop();
 	}	
